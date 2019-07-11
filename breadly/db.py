43f46d0d8d9914848b4ce82b1cd21d7c945f816a
chrisbrake <chris.brake@gmail.com>
2019-07-10 22:28:42 -0600

    seed_cultrue ingredients

---
+++ breadly/db.py
@@ -0,0 +1,33 @@
+seed_cultrue = [
+    { 
+        'stage' :'Day 1', 
+        'ingredients': { 
+            'flour': { 'description': 'coarse whole wheat or whole rye', 'grams': 120.5 }, 
+            'pineapple juice': { 'description': 'unsweetened', 'grams': 113 } 
+            } 
+    }, { 
+        'stage' :'Day 2', 
+        'ingredients': { 
+            'flour': { 
+                'description': 'bread flour', 
+                'grams': 64 
+                }, 
+            'pineapple juice': { 
+                'description': 'unsweetened', 
+                'grams': 57 
+                } 
+            } 
+    }, { 
+        'stage' :'Day 3+', 
+        'ingredients': { 
+            'flour': { 
+                'description': 'bread flour', 
+                'grams': 128 
+                }, 
+            'pineapple juice': { 
+                'description': 'unsweetened', 
+                'grams': 57 
+                } 
+            } 
+    },
+]
